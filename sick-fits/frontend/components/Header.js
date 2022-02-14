6ca29531b201ddbd81021225675c496202a74452
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2022-02-14 13:16:37 +0200

    06 - An Intro to Styled Components and CSS
    Add a couple styled components to the Header component

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,16 +1,51 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  background: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  transform: skew(-7deg);
+  z-index: 2;
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    align-items: center;
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+  }
+`
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
+        <Logo>
         <Link href="/">Sick fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
