c83938d344d243320c09a18a43357a2c4471518f
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2022-02-14 14:46:49 +0200

    08 - Visualizing Route Changes
    Add nprogress
    Use custom styles for nprogress

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        {/*<Head />*/}
+        <Head />
         <body>
           <Main />
           <NextScript />
