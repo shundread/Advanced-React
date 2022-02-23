53ab21627baaf2c5ec735d09a390bccbfcaaafbf
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2022-02-23 13:52:50 +0200

    15 - Creating our Products Data Type

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -4,6 +4,7 @@ import { config, createSchema } from "@keystone-next/keystone/schema";
 import { withItemData, statelessSessions } from "@keystone-next/keystone/session";
 
 import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
 
 const databaseURL = process.env.DATABASE_URL || "mongodb://localhost:27017/sick-fits-keystone";
 
@@ -35,7 +36,8 @@ export default withAuth(config({
     // TODO: Add data seeding here
   },
   lists: createSchema({
-    User
+    User,
+    Product
     // Schema items go in here
   }),
   ui: {
