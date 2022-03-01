722daccf53a12a12658b492b74950efabca5b578
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2022-03-01 09:47:55 +0200

    31 - Evicting items from the Apollo Cache

--- sick-fits/frontend/components/DeleteProduct.js
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -5,14 +5,18 @@ const DELETE_PRODUCT_MUTATION = gql`
   mutation DELETE_PRODUCT_MUTATION($id: ID!) {
     deleteProduct(id: $id) {
       id
-      name
     }
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
 export function DeleteProduct({ id, children }) {
   const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
     variables: { id },
+    update,
   });
   function handleClick() {
     if (confirm("Are you sure you want to delete this item?")) {
