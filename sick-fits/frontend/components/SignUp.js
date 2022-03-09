201a79ed87824463cd927021b5312b9d9579e2ce
Thiago Chaves de Oliveira Horta <thiago.coh@vincit.fi>
2022-03-09 12:18:30 +0200

    39 - Creating a Sign Up Flow

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -6,54 +6,60 @@ import { CURRENT_USER_QUERY } from "../lib/useUser";
 import { ErrorMessage } from "./ErrorMessage";
 
 const SIGN_IN_MUTATION = gql`
-  mutation SIGN_IN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        sessionToken
-        item {
-          id
-          name
-          email
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+  mutation SIGN_UP_USER($email: String!, $name: String!, $password: String!) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
     }
   }
 `;
 
 const DefaultState = {
   email: "",
+  name: "",
   password: "",
 };
 
-export function SignIn() {
+export function SignUp() {
   const { inputs, handleChange, resetForm } = useForm(DefaultState);
 
-  const [signin, { data, error, loading }] = useMutation(SIGN_IN_MUTATION, {
+  const [signup, { data, error, loading }] = useMutation(SIGN_IN_MUTATION, {
     variables: inputs,
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }], <- The operation creates the user but does not sign it
   });
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log(event, inputs);
 
-    // Send the email and password to the graphqlAPI
-    const result = await signin();
+    // Send the email, name and password to the graphqlAPI
+    const result = await signup().catch(console.error);
+    console.log("Result is", result);
     resetForm();
   }
 
   return (
     <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Into Your Account</h2>
+      <h2>Sign Up For an Account</h2>
       <ErrorMessage error={error /* GraphQL error */} />
-      <ErrorMessage
-        error={data?.authenticateUserWithPassword /* Authentication error */}
-      />
+      <ErrorMessage error={data?.createUser /* Authentication error */} />
       <fieldset disabled={loading}>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please go ahead and sign in
+          </p>
+        )}
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
         <label htmlFor="email">
           Email
           <input
@@ -76,7 +82,7 @@ export function SignIn() {
             onChange={handleChange}
           />
         </label>
-        <button type="submit">Sign in!</button>
+        <button type="submit">Sign Up!</button>
       </fieldset>
     </Form>
   );
